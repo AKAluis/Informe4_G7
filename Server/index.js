cb17cdbb42518b930eb283c6101ce88f92582f1f
AKAluis <akaluiszsss@gmail.com>
2024-09-13 18:20:09 -0600

    First Commit

---
+++ Server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const port = 3000;
+
+app.use(cors());
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
+
+app.listen(port, () => {
+  console.log("Servidor ejecutándose en el puerto " + port);
+});
